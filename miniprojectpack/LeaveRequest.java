878ef932b1a2fc1c5b510c48acb3e4bfaeba1023
janhviiiiii <janhvibaraskar03@gmail.com>
2024-10-21 00:44:24 +0530

    added LeaveRequest.java code

--- miniprojectpack/LeaveRequest.java
+++ miniprojectpack/LeaveRequest.java
@@ -1,5 +1,54 @@
 package miniprojectpack;
 
+
 public class LeaveRequest {
+    private String employeeName;
+    private String leaveType;
+    private int days;
+    private boolean isApproved;
+
+    public LeaveRequest(String employeeName, String leaveType, int days)
+    {
+        this.employeeName=employeeName;
+        this.leaveType=leaveType;
+        this.days=days;
+        this.isApproved=false; //initially false
+    }
+
+    //getter and setter methods
+    public String getEmployeeName()
+    {
+        return employeeName;
+    }
+
+    public String getLeaveType()
+    {
+        return leaveType;
+    }
+
+    public int getDays()
+    {
+        return days;
+    }
+
+    public boolean isApproved()
+    {
+        return isApproved;
+    }
+
+    public void approve()
+    {
+        this.isApproved=true;
+    }
+
+    public String toString()
+    {
+        return "LeaveRequest{" +
+                "employeeName='" + employeeName + '\'' +
+                ", leaveType='" + leaveType + '\'' +
+                ", days=" + days +
+                ", isApproved=" + isApproved +
+                '}';
+    }
     
 }
